b4b9b9c4936b852bcb434b6e25f70dbade6a2243
MelanieKent <melanie.s.kent@gmail.com>
2023-02-10 21:20:39 -0800

    Added tapping feature to place marker

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 export default function MapScreen() {
+
+  const [markerObjects, setMarkerObjects] = useState([])
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
@@ -14,17 +17,28 @@ export default function MapScreen() {
             latitudeDelta: 0.01,
             longitudeDelta: 0.004,
           }}
-
-          customMapStyle={mapStyle}>
-          <Marker
-            draggable coordinate={{
-              latitude: 49.26140,
-              longitude: -123.24858,
-            }}
-            image={require('../assets/Marker.png')}
-            title={'Title of the marker goes here'}
-            description={'Description of the marker goes here'}
-          />
+          customMapStyle={mapStyle}
+          onPress={e => {
+            setMarkerObjects([
+              {
+                lat: e.nativeEvent.coordinate.latitude,
+                lnt: e.nativeEvent.coordinate.longitude,
+              }
+            ])
+          }}
+          >
+            {
+              markerObjects.map((marker, index) => (
+                <Marker
+                  key={index}
+                  image={require('../assets/Marker.png')}
+                  title={'Title of the marker goes here'}
+                  description={'Description of the marker goes here'}
+                  draggable coordinate={
+                  {latitude: marker.lat, longitude: marker.lnt}}
+                />
+              ))
+            }
         </MapView>
       </View>
     </SafeAreaView>
@@ -32,7 +46,7 @@ export default function MapScreen() {
 };
 
 const mapStyle = [
-  //can add styles here
+  //can add styles here if desired
 ];
 const styles = StyleSheet.create({
   container: {
